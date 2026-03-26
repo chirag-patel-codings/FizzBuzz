476b722e6a1abd8f11e93efb9b5e8700bbef2b94
Chirag Patel <chirag.patel.learning@gmail.com>
2026-03-26 17:17:42 -0500

    FizzBuzz Completed

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[] inputs = new int[] {3, 5, 15, 16};
+
+foreach (int input in inputs)
+{
+    Console.WriteLine(FizzBuzz(input));
+}
+
+static string FizzBuzz (int num)
+{
+    return (num % 3 == 0) ? "fizz" : (num % 5 == 0 ? "buzz" : (num % 3 == 0 || num % 5 == 0) ? "fizzbuzz" : "NotAFizzBuzz");
+}
